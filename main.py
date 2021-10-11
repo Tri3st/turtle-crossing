afe13364df5c20a25af055fe53992134483ff16b
Martin van Diest <martin.van.diest@gmail.com>
2021-10-11 18:47:22 +0200

    First steps PLayer class

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.bgcolor("grey")
+screen.title("turtle crossing")
+
+player1 = Player()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    screen.onkeypress(player1.move_turtle, "Up")
+
+    screen.exitonclick()
